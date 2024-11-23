3f3b08f55845fd98787907db3408be572517e1bd
krowsmig <186128055+krowsmig@users.noreply.github.com>
2024-11-24 06:38:53 +0900

    Update jndk_script.js

--- js/jndk_script.js
+++ js/jndk_script.js
@@ -2,13 +2,15 @@ $(document).ready(function(){
 
 
 				var pd_btm  = $("#img_title_1 .img_title_div > .pd_tp_bm_300").outerHeight() - $("#img_title_1 .img_title_div > .pd_tp_bm_300").height();
-
 				var top_height = $("#gnb").outerHeight() + $("#main").outerHeight() + $("#target").outerHeight() - ( pd_btm / 2 );
-				var now = $(window).scrollTop();
-				console.log(top_height);
-
 				var top_height_2 = top_height + $("#img_title_1").outerHeight();
 				
+				$(window).resize(function(){
+					pd_btm  = $("#img_title_1 .img_title_div > .pd_tp_bm_300").outerHeight() - $("#img_title_1 .img_title_div > .pd_tp_bm_300").height();
+					top_height = $("#gnb").outerHeight() + $("#main").outerHeight() + $("#target").outerHeight() - ( pd_btm / 2 );
+					top_height_2 = top_height + $("#img_title_1").outerHeight();
+				});
+
 				
 				$(window).scroll(function(){
 					if ($(this).scrollTop() > top_height) {
@@ -29,7 +31,8 @@ $(document).ready(function(){
 						
 					}
 				
-        });
-
+				
+				
+				});	
 					
 			});
